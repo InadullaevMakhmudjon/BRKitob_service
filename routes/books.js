9cacb4acc134ffc5c5456f35a654bc43fa409640
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-25 00:10:36 +0500

    Books and Gifts routes replaced with query prop

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 import books from '../controller/books';
-import { validationBook, validateImage } from '../utils/validations/books';
+import validationBook, { create, update } from '../utils/validations/books';
 
 const router = Router();
 
 router.get('/', books.getAll);
 router.get('/:id', books.get);
-router.get('/:language', books.getByTitle);
-router.post('/image', validateImage, books.appendImage);
-router.post('/', validationBook, books.create);
-router.post('/:id', validationBook, books.update);
+router.post('/', create, validationBook, books.create);
+router.post('/:id', update, validationBook, books.update);
 
 export default router;
