1161f08ab5c2d022731af74017a8c6021facb213
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-25 00:09:28 +0500

    Book gets by query prop

--- controller/books.js
+++ controller/books.js
@@ -4,14 +4,20 @@ import hook from '../utils/hook';
 const include = [{
   model: models.Image,
   as: 'images',
-  attributes: { exclude: ['bookId', 'BookId'] },
+  attributes: { exclude: ['bookId', 'BookId', 'id'] },
 }];
+
+const find = (where, single) => new Promise((res, rej) => {
+  models.Book[single ? 'findOne' : 'findAll']({ where, include })
+    .then(res).catch(rej);
+});
 
 export default {
   async getAll(req, res) {
     try {
-      const users = await models.Book.findAll();
-      res.status(200).json(users);
+      const { title_kr, title_lat } = req.query;
+      const books = await find(req.query, title_kr || title_lat);
+      res.status(200).json(books);
     } catch (e) {
       res.status(502).json(e);
     }
@@ -21,30 +27,31 @@ export default {
       .then((book) => res.status(200).json(book))
       .catch((error) => res.status(502).json(error));
   },
-  async getByTitle(req, res) {
+  async create(req, res) {
     try {
-      const { title } = req.query;
-      const { language } = req.params;
-      if (language && title) {
-        const book = await models.Book.findOne({
-          where: { [`title_${language}`]: title },
-          include,
-        });
-        res.status(200).json(book);
-      }
-    } catch (e) {
-      res.status(502).json(e);
+      const { id: bookId } = await models.Book.create(req.book);
+      await Promise.all(req.book.images.map((url) => models.Image.create({ bookId, url })));
+      res.sendStatus(201);
+      hook();
+    } catch (error) {
+      res.status(502).json(error);
     }
   },
-  create(req, res) {
-    models.Book.create(req.book)
-      .then(() => { res.sendStatus(201); hook(); })
-      .catch((error) => res.status(502).json(error));
-  },
-  update(req, res) {
-    models.Book.update(req.book, { where: { id: req.params.id } })
-      .then(() => res.sendStatus(200))
-      .catch((error) => res.status(502).json(error));
+  async update(req, res) {
+    try {
+      const book = {};
+      Object.keys(req.book).forEach((key) => {
+        if (req.book[key]) { book[key] = req.book[key]; }
+      });
+      await models.Book.update(req.book, { where: { id: req.params.id } });
+      if (book.images) {
+        await models.Image.destroy({ where: { bookId: req.params.id } });
+        await Promise.all([book.images.map((url) => models.Image.create({ url, bookId: req.params.id }))]);
+      }
+      res.sendStatus(200);
+    } catch (error) {
+      res.status(502).json(error);
+    }
   },
   appendImage(req, res) {
     models.Image.create(req.bookImage)
